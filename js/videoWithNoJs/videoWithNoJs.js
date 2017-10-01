ad108711d86498346c09d4b9db6a3e5cdd539530
Jan <jan.dziewulski@onet.com.pl>
2017-10-01 23:25:18 +0200

    good way
    JEST DOBRZE

--- js/videoWithNoJs/videoWithNoJs.js
+++ js/videoWithNoJs/videoWithNoJs.js
@@ -1,21 +1,12 @@
-                   $(document).ready(function(){
-  sizeTheVideo();
-  $(window).resize(function(){
-    sizeTheVideo();
-  });  
-});
+$(window).on('resize', function() {
 
-function sizeTheVideo(){
-  // - 1.78 is the aspect ratio of the video
-// - This will work if your video is 1920 x 1080
-// - To find this value divide the video's native width by the height eg 1920/1080 = 1.78
-  var aspectRatio = 1.78;
-  
+    // Same code as on load        
+    var aspectRatio = 1.25;
     var video = $('#videoWithJs iframe');
     var videoHeight = video.outerHeight();
     var newWidth = videoHeight*aspectRatio;
-		var halfNewWidth = newWidth/2;
-    
-  //Define the new width and centrally align the iframe
-  video.css({"width":newWidth+"px","left":"50%","margin-left":"-"+halfNewWidth+"px"});
-}
+    var halfNewWidth = newWidth/6;
+
+    video.css({"width":newWidth+"px","left":"50%","margin-left":"-"+halfNewWidth+"px"});
+
+});
